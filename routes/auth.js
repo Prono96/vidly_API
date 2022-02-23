168a6311d91500c6ce6a6af1b876329d3cd5df6a
Promise <promiseprince01@gmail.com>
2022-02-23 11:30:16 +0100

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+const config = require('config'); 
+const Joi = require('Joi');
+const {User} = require('../models/user');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const _ = require('lodash');
+
+const express = require('express');
+const router = express.Router();
+
+
+// Post a user 
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne( {email: req.body.email} );
+  if(!user) res.status(400).send('invalid email or password');
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if(!validPassword) res.status(400).send('invalid email or paswword');
+
+  // const token = user.generateAuthToken();
+  const token = jwt.sign({_id: user._id }, config.get('jwtPrivateKey'));
+  res.send(token);
+  console.log("you are logged in already")
+});
+
+// Validation function 
+function validate(req) {
+  const schema = {
+    email: Joi.string().min(3).max(255).required().email(),
+    password: Joi.string().min(3).max(255).required()
+  };
+
+  return Joi.validate(req, schema);
+}
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,47 @@
+const auth = require('../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const {User, validate} = require('../models/user');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const _ = require('lodash');
+
+const express = require('express');
+const router = express.Router();
+
+// Get all users 
+router.get('/me', async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
+
+// Post a user 
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne( {email: req.body.email} );
+  if(user) res.status(400).send('This user already exist in our database');
+
+  user = new User (_.pick(req.body, ['name', 'password', 'email']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  await user.save();
+
+  // const token = user.generateAuthToken();
+  const token = jwt.sign({_id: user._id }, config.get('jwtPrivateKey'));
+  res.header('x-auth-token', token).send( _.pick(user, ['name', 'email']));
+});
+
+// Delete a user 
+router.delete('/:id', async (req, res) => {
+  const user = await User.findByIdAndRemove(req.params.id);
+
+  if (!user) return res.status(404).send('The user with the given ID was not found.');
+
+  res.send(user);
+  console.log(user);
+});
+
+module.exports = router;
