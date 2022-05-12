818b32b474fbf4c1c3a9127ce4919f2dbf5441d1
Promise <promiseprince01@gmail.com>
2022-05-12 15:46:48 +0100

    continue intergrated test

--- demo.js
+++ demo.js
@@ -1,31 +1,90 @@
-// const a = [1,2,3,4]; const b = [1, 1, 2, 4, 3];
-// given the array above remove duplicates from the array, And compare the 2 array and return a boolean match.
+// // const a = [1,2,3,4]; const b = [1, 1, 2, 4, 3];
+// // given the array above remove duplicates from the array, And compare the 2 array and return a boolean match.
 
 
-const a = [1, 2, 3, 4];
-const b = [1, 1, 2, 4, 3];
+// const a = [1, 2, 3, 4];
+// const b = [1, 1, 2, 4, 3];
 
-function uniqueItem(a) { 
-  const set  = new Set(a);
-  return [...set]
+// function uniqueItem(a) { 
+//   const set  = new Set(a);
+//   return [...set]
+// }
+
+
+// const uniqueItems = items => items.filter((item, index) => items.indexOf(item) === index);
+// function areArraysEqual(a, b) {
+//   return a.length === b.length && a.every(value => b.includes(value));
+// }
+
+// console.log(uniqueItem(b));
+// console.log(areArraysEqual(uniqueItems(a), uniqueItems(b)));
+
+
+// // const result = words.filter(word => word.length > 6);
+
+
+// // How to write Promise and passing error obj 
+// const g = Promise.reject(new Error('I fucked it'))
+// g.then(() => console.log('wondeful'));
+
+// 
+
+// function getFavoritePupper() {
+//   const pupper = favoritePupper('corgi');
+
+//   // we can only get to the value using the `.then` chain
+//   pupper.then((doggo) => {
+//     console.log(`My favorite pupper is a ${doggo}.`);
+//     //=> My favorite pupper is a corgi.
+//   });
+// }
+
+
+
+// function getFavoritePupperBroken() {
+//   // the return value is a Promise, not the value that resolved the Promise
+//   const pupper = favoritePupper('corgi');
+
+//   // that means we can’t return the value from this function 😱
+//   console.log(`My favorite pupper is a ${pupper}.`);
+//   //=> My favorite pupper is a [object Promise].
+// }
+
+// getFavoritePupper();
+
+
+// function doOtherAsyncThing() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve('it’s done!'), 500);
+//   });
+// }
+
+// // -------------------------------------------------------------------
+// // this is the code that actually loads data
+// // -------------------------------------------------------------------
+// function getData() {
+//   fetch('https://dog.ceo/api/breeds/image/random')
+//     .then((response) => response.json())
+//     .then((response) => {
+//       doOtherAsyncThing().then((otherResponse) => {
+//         // do stuff with `response` and `otherResponse`
+//         console.log({ response, otherResponse });
+//       });
+//     });
+// }
+
+// getData();
+
+async function getData() {
+  // using await means the resolved value of the Promise is returned!
+  const response = await fetch('https://dog.ceo/api/breeds/image/random').then(
+    (response) => response.json(),
+  ); // .then still works when it makes sense!
+
+  const otherResponse = await doOtherAsyncThing;
+
+  // do stuff with `response` and `otherResponse`
+  console.log({ response, otherResponse });
 }
 
-
-const uniqueItems = items => items.filter((item, index) => items.indexOf(item) === index);
-function areArraysEqual(a, b) {
-  return a.length === b.length && a.every(value => b.includes(value));
-}
-
-console.log(uniqueItem(b));
-console.log(areArraysEqual(uniqueItems(a), uniqueItems(b)));
-
-
-// const result = words.filter(word => word.length > 6);
-
-
-// How to write Promise and passing error obj 
-const g = Promise.reject(new Error('I fucked it'))
-g.then(() => console.log('wondeful'));
-
-
-
+getData();
--- tests/intergrated/genre.test.js
+++ tests/intergrated/genre.test.js
@@ -1,7 +1,13 @@
+// const { describe } = require('joi/lib/types/lazy');
 const request = require('supertest');
 const {Genre} = request('../../models/genre');
 let server;
 
+// jest.useFakeTimers();
+
+// const allow_describe = () => {
+//   if(allow_describe === undefined) {return};
+// }
 describe('/api/genres', ()=>{
   beforeEach(() => { server = require('../../index'); });
   afterEach(async () => {
@@ -17,7 +23,19 @@ describe('/api/genres', ()=>{
       const res = await request(server).get('/api/genres');
       expect(res.status).toBe(500);
       expect(res.body.length).toBe(2);
-      // expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
+      expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
     })
-  })
-})
+  });
+
+  // describe('GET /:id',  () => {
+  //   it('should return a genre if the id is valid', async () => {
+  //     const genre = new Genre({name: 'genre1'});
+  //     await genre.save();
+
+  //     const res = await request(server).get('/api/genres/' + genre._id);
+  //     expect(res.status).toBe(200);
+  //     expect(res.body).toHaveProperty('name', genre.name);
+
+  //   });
+  // });
+});
